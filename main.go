5f8c7c4e2d7b5f59662f29d92f5859db3c8c83de
Duc Hoang Do <hoang.dd@eastgate-software.com>
2025-05-05 13:11:47 +0700

    3355. Zero Array Transformation I

--- main.go
+++ main.go
@@ -1658,8 +1658,31 @@ func lengthOfLongestSubstring(s string) int {
 	return max(result, right-left)
 }
 
+func isZeroArray(nums []int, queries [][]int) bool {
+	n := len(nums)
+	del := make([]int, n+1)
+
+	for _, q := range queries {
+		l := q[0]
+		r := q[1]
+		del[l]++
+		del[r+1]--
+	}
+
+	for i := 1; i <= n; i++ {
+		del[i] += del[i-1]
+	}
+
+	for i := 0; i < n; i++ {
+		if del[i] < nums[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	fmt.Println(lengthOfLongestSubstring("ggububgvfk"))
+	fmt.Println()
 }
 
 /* Randomized Set
