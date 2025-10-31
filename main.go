85cfaa715efdfca29a3df8e2392c1750b1756577
Duc Hoang, Do <hoang.dd@eastgate-software.com>
2025-10-31 19:33:57 +0700

    3289. The Two Sneaky Numbers of Digitville

--- main.go
+++ main.go
@@ -1746,6 +1746,34 @@ func isValid(word string) bool {
 
 }
 
+func getSneakyNumbers(nums []int) []int {
+	n := len(nums) - 2
+	y := 0
+	for _, x := range nums {
+		y ^= x
+	}
+	for i := 0; i < n; i++ {
+		y ^= i
+	}
+	lowBit := y & -y
+	x1, x2 := 0, 0
+	for _, x := range nums {
+		if x&lowBit != 0 {
+			x1 ^= x
+		} else {
+			x2 ^= x
+		}
+	}
+	for i := 0; i < n; i++ {
+		if i&lowBit != 0 {
+			x1 ^= i
+		} else {
+			x2 ^= i
+		}
+	}
+	return []int{x1, x2}
+}
+
 func main() {
 	fmt.Println()
 }
